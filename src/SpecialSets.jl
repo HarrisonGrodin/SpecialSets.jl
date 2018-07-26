036f3f1c8199f12a12b61047a366217e98967503
Harrison Grodin <harrison.grodin@gmail.com>
2018-07-26 16:49:08 -0400

    Add abstract types and methods

--- src/SpecialSets.jl
+++ src/SpecialSets.jl
@@ -1,3 +1,26 @@
 module SpecialSets
 
+export SpecialSet, InfiniteSet
+export ∅
+
+
+abstract type SpecialSet end
+Base.in(x, ::SpecialSet) = false
+
+
+const ∅ = Set()
+Base.intersect(a::AbstractSet, b::SpecialSet) = Set([x for x ∈ a if x ∈ b])
+Base.intersect(b::SpecialSet, a::AbstractSet) = Base.intersect(a, b)
+
+"""
+    InfiniteSet <: SpecialSet
+
+Set of infinite size.
+"""
+abstract type InfiniteSet <: SpecialSet end
+Base.eltype(::InfiniteSet) = Any
+
+
+include("show.jl")
+
 end # module
